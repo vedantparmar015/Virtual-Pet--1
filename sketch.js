f32c5f87782449ef72e77b5e6fd1b196d1931fe1
vedantparmar015 <69168878+vedantparmar015@users.noreply.github.com>
2020-11-27 14:13:05 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+//Create variables here
+var dog
+var happyDog
+var database
+var foodS
+var foodStock
+function preload()
+{
+  //load images here
+  dogHungry = loadImage("images/dogImg.png");
+  dogHappy = loadImage("images/dogImg1.png");
+
+}
+
+function setup() {
+  createCanvas(500,500);
+  dog = createSprite(250,250,50,50);
+  dog.addImage(dogHungry);
+  database = firebase.database();
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+}
+
+
+function draw() {  
+  background(46, 139, 87)
+  if(keyWentUp(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogHappy);
+  }
+
+  drawSprites();
+
+  //add styles here
+
+}
+
+function readStock(data){
+  foodS-data.val();
+}
+
+function writeStock(x){
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+
